03d3ab13fa2a8d72d38e41999f4e57b5bf2c97d0
jiyu <han01031@naver.com>
2025-07-24 03:14:41 +0900

    Feat: 자기소개 커스텀 예외 추가

---
+++ src/main/java/com/umc/tomorrow/domain/introduction/exception/code/IntroductionStatus.java
@@ -0,0 +1,29 @@
+package com.umc.tomorrow.domain.introduction.exception.code;
+
+import com.umc.tomorrow.global.common.exception.code.BaseCode;
+import com.umc.tomorrow.global.common.exception.code.BaseCodeInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum IntroductionStatus implements BaseCodeInterface {
+    INTRODUCTION_NOT_FOUND(HttpStatus.NOT_FOUND, "INTRODUCTION404", "해당 자기소개 페이지를 찾을 수 없습니다."),
+    INTRODUCTION_FORBIDDEN(HttpStatus.FORBIDDEN, "INTRODUCTION403", "해당 자기소개 페이지에 대한 권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final boolean isSuccess = false;
+    private final String code;
+    private final String message;
+
+    @Override
+    public BaseCode getCode() {
+        return BaseCode.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(isSuccess)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
