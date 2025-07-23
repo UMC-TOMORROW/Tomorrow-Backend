f9120a22e3a73c0e012bc4b31591dd149279f036
Yeojin0523 <d02jyjeong@naver.com>
2025-07-23 14:02:06 +0900

    Feat : 회원 탈퇴, 회원 복구 api구현

---
+++ src/main/java/com/umc/tomorrow/domain/member/dto/response/RecoverUserResponse.java
@@ -0,0 +1,19 @@
+/**
+ * RecoverUserResponse (회원복구)
+ - patch /api/v1/members/{memberId}/recover
+ * 작성자: 정여진
+ * 생성일: 2025-07-23
+ */
+package com.umc.tomorrow.domain.member.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class RecoverUserResponse {
+    private String status;
+    private LocalDateTime recoveredAt;
+}
