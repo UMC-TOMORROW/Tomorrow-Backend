b9de59f2746601c468fd71988a8990335f19b4d1
이승주 <eun07213@gmail.com>
2025-07-24 02:19:20 +0900

    Fix: 커리어톡 예외처리 주석 추가

--- src/main/java/com/umc/tomorrow/domain/careertalk/exception/CareertalkException.java
+++ src/main/java/com/umc/tomorrow/domain/careertalk/exception/CareertalkException.java
@@ -1,3 +1,8 @@
+/**
+ * 커리어톡 에 대한 예외처리 클래스
+ * 작성자: 이승주
+ * 생성일: 2025-07-24
+ */
 package com.umc.tomorrow.domain.careertalk.exception;
 
 import com.umc.tomorrow.domain.careertalk.exception.code.CareertalkStatus;
