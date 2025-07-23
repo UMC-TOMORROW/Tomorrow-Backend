9ce6dfb428a451f4d531682d3a1909209089819d
Yeojin0523 <d02jyjeong@naver.com>
2025-07-24 00:05:58 +0900

    Fix : ApplicationStatus enum에서 예외 처리 방식개선, ApplicationException 클래스 생성

---
+++ src/main/java/com/umc/tomorrow/domain/application/exception/ApplicationException.java
@@ -0,0 +1,13 @@
+/**
+ * ApplicationException
+ * 작성자: 정여진
+ * 생성일: 2025-07-24
+ */
+package com.umc.tomorrow.domain.application.exception;
+import com.umc.tomorrow.global.common.exception.RestApiException;
+
+public class ApplicationException extends RestApiException {
+    public ApplicationException(ApplicationErrorStatus status) {
+        super(status);
+    }
+}
