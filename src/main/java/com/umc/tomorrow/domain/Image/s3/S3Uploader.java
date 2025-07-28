4a2930584c54a85a73bcf8a999eaa3c42a032119
이승주 <eun07213@gmail.com>
2025-07-28 13:37:07 +0900

    Feat: S3 관련 파일 삭제 기능 추가

--- src/main/java/com/umc/tomorrow/domain/Image/s3/S3Uploader.java
+++ src/main/java/com/umc/tomorrow/domain/Image/s3/S3Uploader.java
@@ -1,3 +1,8 @@
+/**
+ * S3 Image uploader
+ * 작성자: 이승주
+ * 작성일: 2025-07-28
+ */
 package com.umc.tomorrow.domain.Image.s3;
 
 import com.amazonaws.services.s3.AmazonS3;
@@ -15,7 +20,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class S3Uploader {
 
     private final AmazonS3 amazonS3;
-
     private final String bucketName = "umc8-tomorrow-bucket";
 
     public String upload(MultipartFile file, String dirName) {
@@ -32,5 +36,14 @@ public class S3Uploader {
         }
 
         return amazonS3.getUrl(bucketName, fileName).toString();
+    }
+
+    public void delete(String fileUrl) {
+        String key = extractKeyFromUrl(fileUrl); // S3 키만 추출
+        amazonS3.deleteObject(bucketName, key);
+    }
+
+    private String extractKeyFromUrl(String fileUrl) {
+        return fileUrl.substring(fileUrl.indexOf(".com/") + 5); // S3 object key 추출
     }
 }
