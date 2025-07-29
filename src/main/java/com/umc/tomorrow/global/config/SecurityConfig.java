c80566f8ef40ecdbedfb4f76ea51b3ff76310ef0
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2025-07-29 15:50:00 +0900

    Update SecurityConfig.java

--- src/main/java/com/umc/tomorrow/global/config/SecurityConfig.java
+++ src/main/java/com/umc/tomorrow/global/config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig {
                             "/v3/api-docs/**",
                             "/swagger-resources/**",
                             "/webjars/**",
-                            "/swagger-ui.html",
+                            "/swagger-ui.html"
                         ).permitAll()
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated());
