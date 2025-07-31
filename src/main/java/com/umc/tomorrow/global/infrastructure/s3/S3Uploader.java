c3a31f4a1ffefaf1eb1be2ad9d0952945258e87d
이승주 <eun07213@gmail.com>
2025-07-31 15:25:16 +0900

    Feat: 파일 업로드 및 삭제 uploader

---
+++ src/main/java/com/umc/tomorrow/global/infrastructure/s3/S3Uploader.java
@@ -0,0 +1,58 @@
+/**
+ * S3 Image uploader
+ * 작성자: 이승주
+ * 작성일: 2025-07-28
+ */
+package com.umc.tomorrow.global.infrastructure.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.umc.tomorrow.global.common.exception.RestApiException;
+import com.umc.tomorrow.global.common.exception.code.GlobalErrorStatus;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class S3Uploader {
+
+    private AmazonS3 amazonS3;
+    private final String bucketName;
+
+    public S3Uploader(AmazonS3 amazonS3,
+                      @Value("${cloud.aws.s3.bucket}") String bucketName) {
+        this.amazonS3 = amazonS3;
+        this.bucketName = bucketName;
+    }
+
+    public String upload(MultipartFile file, String dirName) {
+        String fileName = dirName + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+
+            amazonS3.putObject(bucketName, fileName, file.getInputStream(), metadata);
+        } catch (IOException e) {
+            throw new RestApiException(GlobalErrorStatus._S3_UPLOAD_ERROR);
+        }
+
+        return amazonS3.getUrl(bucketName, fileName).toString();
+    }
+
+    public void delete(String fileUrl) {
+        String key = extractKeyFromUrl(fileUrl); // S3 키만 추출
+        amazonS3.deleteObject(bucketName, key);
+    }
+
+    private String extractKeyFromUrl(String fileUrl) {
+        String bucketUrl = amazonS3.getUrl(bucketName, "").toString(); // ex: https://bucket.s3.amazonaws.com/
+        return fileUrl.replace(bucketUrl, "");
+    }
+
+}
