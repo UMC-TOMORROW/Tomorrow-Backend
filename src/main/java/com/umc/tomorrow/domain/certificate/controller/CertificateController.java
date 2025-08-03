fc9116dfb14775a8a759dc6c4e459a346d53ffe9
이승주 <eun07213@gmail.com>
2025-08-03 23:47:07 +0900

    Feat: 자격증 조회 목록 API(Get)

--- src/main/java/com/umc/tomorrow/domain/certificate/controller/CertificateController.java
+++ src/main/java/com/umc/tomorrow/domain/certificate/controller/CertificateController.java
@@ -8,14 +8,17 @@ package com.umc.tomorrow.domain.certificate.controller;
 
 import com.umc.tomorrow.domain.certificate.dto.response.CertificateResponse;
 import com.umc.tomorrow.domain.certificate.service.command.CertificateCommandService;
+import com.umc.tomorrow.domain.certificate.service.query.CertificateQueryService;
 import com.umc.tomorrow.global.common.base.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +33,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class CertificateController {
 
     private final CertificateCommandService certificateCommandService;
+    private final CertificateQueryService certificateQueryService;
 
     @PostMapping(value = "{resumeId}/certificates", consumes = "multipart/form-data")
     @Operation(summary = "이력서 자격증 업로드", description = "이력서에 자격증을 업로드합니다.")
@@ -51,5 +55,15 @@ public class CertificateController {
     ) {
         CertificateResponse response = certificateCommandService.deleteCertificate(certificateId);
         return ResponseEntity.ok(BaseResponse.onSuccessDelete(response));
+    }
+
+    @GetMapping("{resumeId}/certificates")
+    @Operation(summary = "이력서 자격증 목록 조회", description = "이력서에 등록된 자격증 목록을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "자격증 조회 성공")
+    public ResponseEntity<BaseResponse<List<CertificateResponse>>> getCertificates(
+            @PathVariable Long resumeId
+    ){
+        List<CertificateResponse> certificates = certificateQueryService.getCertificatesByResumeId(resumeId);
+        return ResponseEntity.ok(BaseResponse.onSuccess(certificates));
     }
 }
