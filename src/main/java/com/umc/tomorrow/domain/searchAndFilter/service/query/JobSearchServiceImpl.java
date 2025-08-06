964112f44fbe2dd1046f26de0c87be608481d534
jiyu <han01031@naver.com>
2025-08-06 19:25:53 +0900

    Feat: active상태인 모든 공고 조회

--- src/main/java/com/umc/tomorrow/domain/searchAndFilter/service/query/JobSearchServiceImpl.java
+++ src/main/java/com/umc/tomorrow/domain/searchAndFilter/service/query/JobSearchServiceImpl.java
@@ -1,9 +1,11 @@
 package com.umc.tomorrow.domain.searchAndFilter.service.query;
 
 import com.umc.tomorrow.domain.job.entity.Job;
+import com.umc.tomorrow.domain.job.repository.JobRepository;
 import com.umc.tomorrow.domain.searchAndFilter.converter.JobSearchConverter;
 import com.umc.tomorrow.domain.searchAndFilter.dto.request.JobSearchRequestDTO;
 import com.umc.tomorrow.domain.searchAndFilter.dto.response.JobSearchResponseDTO;
+import com.umc.tomorrow.domain.searchAndFilter.repository.JobJpaRepository;
 import com.umc.tomorrow.domain.searchAndFilter.repository.JobSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class JobSearchServiceImpl implements JobSearchService {
 
+    private final JobJpaRepository jobJpaRepository;
     // 일자리 검색 쿼리를 처리하는 Repository
     private final JobSearchRepository jobSearchRepository;
 
@@ -36,6 +39,18 @@ public class JobSearchServiceImpl implements JobSearchService {
         List<Job> jobs = jobSearchRepository.searchJobs(requestDTO);
 
         // Job → JobSearchResponseDTO 변환 후 리스트로 반환
+        return jobs.stream()
+                .map(converter::toResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 검색 조건을 기반으로 일자리 목록을 조회하고 DTO로 변환하여 반환
+     * @return 조건에 맞는 일자리 목록의 응답 DTO 리스트
+     */
+    @Override
+    public List<JobSearchResponseDTO> getAllActiveJobs() {
+        List<Job> jobs = jobJpaRepository.findByIsActiveTrue(); // 필터 없는 전체 조회
         return jobs.stream()
                 .map(converter::toResponseDTO)
                 .collect(Collectors.toList());
