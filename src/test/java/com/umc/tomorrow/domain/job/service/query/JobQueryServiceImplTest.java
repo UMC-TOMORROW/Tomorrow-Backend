df0b2d060d0af8a8bb172cbb7b76c72015bd550e
jiyu <han01031@naver.com>
2025-08-12 09:35:25 +0900

    test: 일자리 검색 및 필터 테스트

---
+++ src/test/java/com/umc/tomorrow/domain/job/service/query/JobQueryServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.umc.tomorrow.domain.job.service.query;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobQueryServiceImplTest {
+
+    @Test
+    void getMyPosts() {
+    }
+
+    @Test
+    void getJobDetail() {
+    }
+}
