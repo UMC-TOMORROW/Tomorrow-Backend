b82ebf203e8e3708d9b9587611a5ffb1522c7030
이승주 <eun07213@gmail.com>
2025-08-13 19:46:57 +0900

    Feat: 멤버 타입 및 필드 추가

---
+++ src/main/java/com/umc/tomorrow/domain/member/entity/MemberType.java
@@ -0,0 +1,11 @@
+/**
+ * 구인, 구직 role enum
+ * 작성자: 이승주
+ * 작성일: 2025-08-13
+ */
+package com.umc.tomorrow.domain.member.entity;
+
+public enum MemberType {
+    EMPLOYER,   // 구인자
+    JOB_SEEKER  // 구직자
+}
