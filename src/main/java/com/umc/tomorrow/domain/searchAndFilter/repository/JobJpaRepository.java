1dd53b178500cb4a8a7a415d1570bf5558b05673
jiyu <han01031@naver.com>
2025-08-15 21:44:38 +0900

    Refactor: isActive -> postStatus

--- src/main/java/com/umc/tomorrow/domain/searchAndFilter/repository/JobJpaRepository.java
+++ src/main/java/com/umc/tomorrow/domain/searchAndFilter/repository/JobJpaRepository.java
@@ -1,10 +1,11 @@
 package com.umc.tomorrow.domain.searchAndFilter.repository;
 
 import com.umc.tomorrow.domain.job.entity.Job;
+import com.umc.tomorrow.domain.job.enums.PostStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface JobJpaRepository extends JpaRepository<Job, Long> {
-    List<Job> findByIsActiveTrue();
+    List<Job> findByStatus(PostStatus status);
 }
